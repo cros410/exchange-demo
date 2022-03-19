6a9d019c286f460053a7b0dea1787ee335cbe4b5
Christian <cros410@gmail.com>
2022-03-19 09:17:47 -0500

    Base project

---
+++ src/main/java/com/example/bcpexchange/BcpExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.example.bcpexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcpExchangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BcpExchangeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bcpexchange/BcpExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bcpexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcpExchangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
